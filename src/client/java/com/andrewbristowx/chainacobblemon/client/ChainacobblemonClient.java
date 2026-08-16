eda911fb9a58cb415cfd82ba8e5ad74978b1b253
andrewBristowx <138131314+andrewBristowx@users.noreply.github.com>
2026-08-16 11:23:48 -0500

    feat: enable Chaina NPC skin renderer

--- src/client/java/com/andrewbristowx/chainacobblemon/client/ChainacobblemonClient.java
+++ src/client/java/com/andrewbristowx/chainacobblemon/client/ChainacobblemonClient.java
@@ -1,14 +1,20 @@
 package com.andrewbristowx.chainacobblemon.client;
 
 import com.andrewbristowx.chainacobblemon.client.emote.ChatEmoteController;
+import com.andrewbristowx.chainacobblemon.client.gameplay.ChainaNpcRenderer;
 import com.andrewbristowx.chainacobblemon.client.gameplay.GameplayClient;
+import com.andrewbristowx.chainacobblemon.client.gameplay.NpcSkinClient;
 import com.andrewbristowx.chainacobblemon.client.systems.SystemsClient;
+import com.andrewbristowx.chainacobblemon.registry.ChainaRegistries;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 
 public final class ChainacobblemonClient implements ClientModInitializer {
     @Override public void onInitializeClient() {
         ChatEmoteController.initialize();
         SystemsClient.initialize();
         GameplayClient.initialize();
+        NpcSkinClient.initialize();
+        EntityRendererRegistry.register(ChainaRegistries.CHAINA_NPC, ChainaNpcRenderer::new);
     }
 }
