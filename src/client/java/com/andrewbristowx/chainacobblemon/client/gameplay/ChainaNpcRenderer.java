57593ed1e20341b871e61583e07f4cad27fc5fd3
andrewBristowx <138131314+andrewBristowx@users.noreply.github.com>
2026-08-16 11:37:53 -0500

    feat: render Chaina NPC skins with correct wide or slim model

--- src/client/java/com/andrewbristowx/chainacobblemon/client/gameplay/ChainaNpcRenderer.java
+++ src/client/java/com/andrewbristowx/chainacobblemon/client/gameplay/ChainaNpcRenderer.java
@@ -1,6 +1,5 @@
 package com.andrewbristowx.chainacobblemon.client.gameplay;
 
-import com.andrewbristowx.chainacobblemon.Chainacobblemon;
 import com.andrewbristowx.chainacobblemon.gameplay.ChainaNpcEntity;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.MobEntityRenderer;
@@ -10,13 +9,16 @@ import net.minecraft.util.Identifier;
 
 /** Player-model renderer for automatically discovered Chaina NPC skins. */
 public final class ChainaNpcRenderer extends MobEntityRenderer<ChainaNpcEntity, PlayerEntityModel<ChainaNpcEntity>> {
-    private static final Identifier FALLBACK = Identifier.of("minecraft", "textures/entity/player/wide/steve.png");
+    private static final Identifier FALLBACK_WIDE = Identifier.of("minecraft", "textures/entity/player/wide/steve.png");
+    private static final Identifier FALLBACK_SLIM = Identifier.of("minecraft", "textures/entity/player/slim/alex.png");
+    private final boolean slim;
 
-    public ChainaNpcRenderer(EntityRendererFactory.Context context) {
-        super(context, new PlayerEntityModel<>(context.getPart(EntityModelLayers.PLAYER), false), 0.5F);
+    public ChainaNpcRenderer(EntityRendererFactory.Context context, boolean slim) {
+        super(context, new PlayerEntityModel<>(context.getPart(slim ? EntityModelLayers.PLAYER_SLIM : EntityModelLayers.PLAYER), slim), 0.5F);
+        this.slim = slim;
     }
 
     @Override public Identifier getTexture(ChainaNpcEntity entity) {
-        return NpcSkinClient.texture(entity.getUuid(), FALLBACK);
+        return NpcSkinClient.texture(entity.getUuid(), slim ? FALLBACK_SLIM : FALLBACK_WIDE);
     }
 }
