254ce61ff44c6728e3725ae1c266cef3142644a9
andrewBristowx <138131314+andrewBristowx@users.noreply.github.com>
2026-08-15 23:10:45 -0500

    chore: mark alpha.1 client baseline

---
+++ src/client/java/com/andrewbristowx/chainacobblemon/client/ChainacobblemonClient.java
@@ -0,0 +1,3 @@
+package com.andrewbristowx.chainacobblemon.client;
+import net.fabricmc.api.ClientModInitializer;
+public final class ChainacobblemonClient implements ClientModInitializer { @Override public void onInitializeClient() { } }
