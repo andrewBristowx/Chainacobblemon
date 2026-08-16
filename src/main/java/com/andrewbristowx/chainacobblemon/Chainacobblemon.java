817b5f283d1bdab0581467bb45522b62dd2a9d5b
andrewBristowx <138131314+andrewBristowx@users.noreply.github.com>
2026-08-15 23:10:20 -0500

    chore: preserve alpha.1 main initializer

---
+++ src/main/java/com/andrewbristowx/chainacobblemon/Chainacobblemon.java
@@ -0,0 +1,14 @@
+package com.andrewbristowx.chainacobblemon;
+import com.andrewbristowx.chainacobblemon.integration.StreamotesServerIntegration;
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+public final class Chainacobblemon implements ModInitializer {
+    public static final String MOD_ID = "chainacobblemon";
+    public static final String VERSION = "0.1.0-alpha.1";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    @Override public void onInitialize() {
+        LOGGER.info("Starting Chainacobblemon {}", VERSION);
+        StreamotesServerIntegration.ensureOfficialChannel();
+    }
+}
