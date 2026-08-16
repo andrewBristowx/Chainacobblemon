ff60016b3768d7eb71be16d456043e1c47ca402c
andrewBristowx <138131314+andrewBristowx@users.noreply.github.com>
2026-08-16 11:41:25 -0500

    chore: report alpha.4 runtime version

--- src/main/java/com/andrewbristowx/chainacobblemon/Chainacobblemon.java
+++ src/main/java/com/andrewbristowx/chainacobblemon/Chainacobblemon.java
@@ -20,7 +20,7 @@ import org.slf4j.LoggerFactory;
 
 public final class Chainacobblemon implements ModInitializer {
     public static final String MOD_ID = "chainacobblemon";
-    public static final String VERSION = "0.2.0-alpha.3+1.21.1";
+    public static final String VERSION = "0.2.0-alpha.4+1.21.1";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
